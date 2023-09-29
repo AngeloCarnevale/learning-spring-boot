ae510aadda595171fe1e5dd8ea98a4af42a9c7c1
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-09-29 16:53:22 -0300

    first commit

---
+++ learning-springboot/src/main/java/com/example/learningspringboot/LearningSpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.example.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class LearningSpringbootApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootApplication.class, args);
+	}
+
+}
